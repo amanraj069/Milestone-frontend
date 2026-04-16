35925cddf5717e9136123000911361e20f078f47
Vanya <iamvanyaawasthi@gmail.com>
2026-04-16 23:31:41 +0530

    implemented tests for frontend

--- src/__tests__/NewQuiz.test.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import NewQuiz from '../pages/Moderator/Quizzes/NewQuiz';
-
-test('renders Add New Skill Quiz header', () => {
-  render(<NewQuiz />);
-  expect(screen.getByText(/Add New Skill Quiz/i)).toBeInTheDocument();
-});
